7abde03ef8ab8a8ea3f46629e45c3930ac93a2ab
pranalivalve1108 <pranalivalve2001@gmail.com>
2024-09-26 16:17:08 +0530

    Updates

---
+++ Images/CertificationsInfo.js
@@ -0,0 +1,51 @@
+export const certificationsInfo = [
+    { 
+        certificationName: 'Full Stack Developement with Python',
+        issuedOn: 'January, 2022',
+        issuingOrganization: 'V3 Data Solutions',
+        imageUrl: '/Images/V3_Solution.jpeg',        
+        credentialUrl: 'https://drive.google.com/file/d/1uD59VtLTiVKWBSEnRs32zJojtrly_sgP/view?usp=drivesdk'
+    },
+    { 
+        certificationName: 'Core Python Programming',
+        issuedOn: 'April, 2022',
+        issuingOrganization: 'Udemy',
+        imageUrl: '/Images/udemy-logo.png', 
+        credentialUrl: 'https://drive.google.com/file/d/16uvgr8Pq4Mhb54OVSFaQ1xyLGRE3tUuN/view?usp=drivesdk'
+    },
+    { 
+        certificationName: 'Python for Beginners',
+        issuedOn: 'November, 2012',
+        issuingOrganization: 'SkillUP by Simplilearn',
+        imageUrl: '/Images/skillup2.png',
+        credentialUrl: 'https://drive.google.com/file/d/179xtTVGf3-1AR-DMigpSJR95-g-vWxx6/view?usp=drivesdk'
+    },
+    { 
+        certificationName: 'HTML Course',
+        issuedOn: 'June, 2021',
+        issuingOrganization: 'CodeLiber',
+        imageUrl: '/Images/CodeLiber.png', 
+        credentialUrl: 'https://drive.google.com/file/d/17UgbTchf15glrc3sv8f-kA2rXXCjTP9b/view?usp=drivesdk'
+    },
+    { 
+        certificationName: 'Programming with Python 3.X',
+        issuedOn: 'December, 2022',
+        issuingOrganization: 'SkillUP by Simplilearn',
+        imageUrl: '/Images/skillup2.png', 
+        credentialUrl: 'https://drive.google.com/file/d/17Ccu8MAnvqTTu9edJ6c7NgseA0PuL2oO/view?usp=drivesdk'
+    },
+    { 
+        certificationName: 'Data Science with Python',
+        issuedOn: 'December, 2022',
+        issuingOrganization: 'SkillUP by Simplilearn',
+        imageUrl: '/Images/skillup2.png', 
+        credentialUrl: 'https://drive.google.com/file/d/17NJaJyxBhCY9XRYfH8I-ClkCmSG6CZN_/view?usp=drivesdk'
+    },
+    { 
+        certificationName: 'World Education Day Fair',
+        issuedOn: 'January, 2022', 
+        issuingOrganization: 'Internshala',
+        imageUrl: '/Images/internshala-logo.png', 
+        credentialUrl: 'https://drive.google.com/file/d/17WSMy0zyDF9ewkyib48YIuhhhDYG53_0/view?usp=drivesdk'
+    },
+];
