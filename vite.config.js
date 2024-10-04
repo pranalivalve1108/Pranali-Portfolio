e15ce1e454717b84a44a66ea86f444a926f0319e
pranalivalve1108 <pranalivalve2001@gmail.com>
2024-10-04 14:36:26 +0530

    1st Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Pranali-Portfolio/",
+  base: "./Pranali-Portfolio/",
   plugins: [react()],
-})
+});
